dca7a4963d50b2a9f8a6282b715bf0a3d34ae233
dsanderson90 <dsanderson3@cnm.edu>
2020-01-04 23:44:43 -0700

    designing shit

--- src/components/PokeSearch.js
+++ src/components/PokeSearch.js
@@ -29,7 +29,7 @@ class PokeSearch extends Component {
     if (result.length === 0) {
       this.setState({
         error: true,
-        result: ''
+        result: '',
       });
       return;
     }
@@ -55,39 +55,44 @@ class PokeSearch extends Component {
           backgroundColor: 'rgb(65,65,65)',
         }}
       >
+        <h1>Poké-Search</h1>
         {result ? (
+          <div>
+            <img
+              style={{
+                margin: '50px 0 0 0',
+                height: '150px',
+              }}
+              src={result[0]['sprites'].animated}
+              alt='gif'
+            />
+            <div>#{result[0]['national_number']}</div>
             <div>
-              <img
-                style={{
-                  margin: '50px 0 0 0',
-                  height: '150px',
-                }}
-                src={result[0]['sprites'].animated}
-                alt='gif'
-              />
-              <div>#{result[0]['national_number']}</div>
-              <div>
-                {result[0].type.map(t => (
-                  <div>{t}</div>
-                ))}
-              </div>
-              <div>hp: {result[0].hp}</div>
-              <div>Attack: {result[0].attack}</div>
-              <div>Speed: {result[0].speed}</div>
-              <div>Defense: {result[0].defense}</div>
-              <div>Special Attack: {result[0].sp_atk}</div>
-              <div>Special Defense: {result[0].sp_def}</div>
+              {result[0].type.map(t => (
+                <div>{t}</div>
+              ))}
             </div>
-          ):
-          <img height='150px' src="https://cdn.dribbble.com/users/815728/screenshots/4046362/ball.gif" alt="loading"/>}
+            <div>hp: {result[0].hp}</div>
+            <div>Attack: {result[0].attack}</div>
+            <div>Speed: {result[0].speed}</div>
+            <div>Defense: {result[0].defense}</div>
+            <div>Special Attack: {result[0].sp_atk}</div>
+            <div>Special Defense: {result[0].sp_def}</div>
+          </div>
+        ) : (
+          <img
+            height='150px'
+            src='https://cdn.dribbble.com/users/815728/screenshots/4046362/ball.gif'
+            alt='loading'
+          />
+        )}
+        {error && <div>Pokemon does not exist.</div>}
         <form>
-          <h1>PokéSearch</h1>
           <input
             name='pokemon'
             placeholder='Search for a pokemon'
             onChange={this.handleChange}
           />
-          {error && <div>Pokemon does not exist.</div>}
           <button
             style={{
               display: 'block',
